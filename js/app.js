bdc03662bec80b5f3a669b7a4a89a6edd13da06c
joseth0r <thor_business@protonmail.com>
2022-06-12 03:44:24 +0200

    tesss

--- js/app.js
+++ js/app.js
@@ -322,8 +322,8 @@ const checkOwner = async (account) => {
       updateStatusText(isOwner, true)
       
       editions = [...editions, ...data.editions]
-    nftname = [...nftname, ...data.nftname]
-    nftimage = [...nftimage, ...data.nftimage]
+    nftname = [...nftname, ...data.name]
+    nftimage = [...nftimage, ...data.image]
       nextPage = data.next_page
     }
 
