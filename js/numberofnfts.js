3ca24a2ac4303aaaebfc2901160f7bc1c5af750b
joseth0r <thor_business@protonmail.com>
2022-06-14 14:35:25 +0200

    test9

--- js/numberofnfts.js
+++ js/numberofnfts.js
@@ -326,20 +326,13 @@ const checkOwner = async (account) => {
 
   function updateStatusText(isOwner, checking) {
     const statusText = document.querySelector('.owner-status');
-    if(checking) {
-      if(isOwner) {
-        statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
-      } else {
-        statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
-      }
-    } else {
+
       if(isOwner) {
         statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
       } else {
         statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
       }
-    }
-    dots = dots === 3 ? 1 : dots + 1;
+    
   }
   
 
