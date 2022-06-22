1b683e55cbbd3ac3d8260e5c75ac4a360f9be8e1
joseth0r <thor_business@protonmail.com>
2022-06-23 01:15:16 +0200

    test12

--- functions/isowner.js
+++ functions/isowner.js
@@ -4,6 +4,10 @@ const CONTRACT = "0x2953399124f0cbb46d2cbacd8a89cf0599974963";
 const AUTH = process.env.NFTPORT_API_KEY;
 const chain = "polygon";
 const include = "metadata";
+const options_os = {
+  method: 'GET',
+  headers: {Accept: 'application/json', 'X-API-KEY': 'bafa0d3c02b54c3dbaf92c66ac2bb250'}
+};
 
 //const tokenarray=["62020157288306137204262585601212871537268194779568533209731806292692472692737","62020157288306137204262585601212871537268194779568533209731806159651565731841"];
 
@@ -67,7 +71,8 @@ const getOwnedNfts = async (wallet, page) => {
       if(nft.contract_address === CONTRACT && (tokenarray.includes(nft.token_id)==true)) { //esto funciona
         editions.push(nft.token_id);
         if (nft.name===""){
-          const missingdata = await fetchMissingData(tokenid);
+          const url_os=`https://api.opensea.io/api/v2/metadata/matic/${CONTRACT}/${nft.token_id}`;
+          const missingdata = await fetchData(url_os, options_os);
           nftname.push(missingdata.name);
           nftimage.push(missingdata.image);
         }
@@ -95,6 +100,26 @@ const getOwnedNfts = async (wallet, page) => {
   }
 }
 
+
+/* ORIGINAL:
+async function fetchData(url, options) {
+  return new Promise((resolve, reject) => {
+    return fetch(url, options).then(res => {
+      const status = res.status;            
+
+      if(status === 200) {
+        return resolve(res.json());
+      } else {
+        console.log(`Fetch failed with status ${status}`);
+        return reject(res.json());
+      }        
+    }).catch(function (error) { 
+      reject(error)
+    });
+  });
+}*/
+
+
 async function fetchData(url, options) {
   return new Promise((resolve, reject) => {
     return fetch(url, options).then(res => {
@@ -115,13 +140,3 @@ async function fetchData(url, options) {
 
 
 
-
-async function fetchMissingData(tokenid){
-  var url_os=`https://api.opensea.io/api/v2/metadata/matic/${CONTRACT}/${tokenid}`;
-
-//const responsemissing=await fetch(url_os,options_os).then(response => response.json());
-return new Promise((resolve, reject) => {
-  return fetch(url_os, options_os).then(resp => resp.json());
-});
-
-}
