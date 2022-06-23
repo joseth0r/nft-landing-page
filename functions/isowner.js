20a7a65331f8a00ab81cc7f0e48ef67d4ef4b0ff
joseth0r <thor_business@protonmail.com>
2022-06-23 16:56:12 +0200

    test

--- functions/isowner.js
+++ functions/isowner.js
@@ -66,7 +66,7 @@ const getOwnedNfts = async (wallet, page) => {
       if(nft.contract_address === CONTRACT && (tokenarray.includes(nft.token_id)==true)) {
         editions.push(nft.token_id);
         if (nft.name===""){
-          var missingdata = missingdatajson.filter( element => element.tokenid == nft.token_id);
+          var missingdata = missingdatajson.filter( element => element.tokenid === nft.token_id);
           nftname.push(missingdata[0].name);
          nftimage.push(missingdata[0].image);
   
