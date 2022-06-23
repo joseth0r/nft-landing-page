90b65e965b9c7a5c400600268f20534f0b4ef718
joseth0r <thor_business@protonmail.com>
2022-06-23 16:08:04 +0200

    testtt

--- functions/isowner.js
+++ functions/isowner.js
@@ -68,8 +68,7 @@ const getOwnedNfts = async (wallet, page) => {
         if (nft.name===""){
           var missingdata = missingdatajson.filter( element => element.tokenid == nft.token_id);
           nftname.push(missingdata[0].name);
-          nftimage.push("hola");
-         // nftimage.push(missingdata[0].image);
+         nftimage.push(missingdata[0].image);
   
           }
           else{       
